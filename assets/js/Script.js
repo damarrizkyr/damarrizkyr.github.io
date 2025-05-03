049ad6505430d6987af448e3f75046d3b5053c68
damarrizkyr <damarrizkyramadhan9@gmail.com>
2025-05-03 11:22:11 +0700

    Add files via upload

---
+++ assets/js/Script.js
@@ -0,0 +1,38 @@
+function myFunction() {
+    alert("Selamat Datang di Website Portofolio Saya");
+  }
+
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterableCards = document.querySelectorAll(".filterable_cards .card_gallery");
+
+// console.log(filterButtons, filterableCards);
+
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    // console.log(e.target);
+
+    filterableCards.forEach(card_gallery => {
+        card_gallery.classList.add("hide");
+
+        if (card_gallery.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+            card_gallery.classList.remove("hide");
+            
+        };
+    });
+};
+
+
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
+
+// Contact
+document.addEventListener("DOMContentLoaded", () => {
+    const contactForm = document.getElementById("contact-form");
+    
+    contactForm.addEventListener("submit", (event) => {
+        event.preventDefault(); // Prevent form from reloading the page
+        alert("Message Sent!");
+        contactForm.reset(); // Clear the form fields
+    });
+});
